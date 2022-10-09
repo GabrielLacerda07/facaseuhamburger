4be22dc7c32e7670ce84a651ad0b5fa6964d2da0
Gabriel Lacerda <gabriellacerdacosta07@gmail.com>
2022-10-08 23:42:13 -0300

    Primeira versão do projeto

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <NavBar :logo='logo_src' :alt='app_name'/>
+  <router-view/>
+  <Footer/>
+</template>
+
+<script>
+  import NavBar from "./components/NavBar.vue";
+  import Footer from "./components/Footer.vue";
+
+  export default{
+    name: 'App', 
+    data(){
+      return{
+        logo_src: 'img/logo.png', 
+        app_name: 'Make your Burger'
+
+      }
+    }, 
+    components:{
+      NavBar, 
+      Footer
+    }
+  }
+</script>
+
+<style>
+*{
+  font-family: Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container{
+  margin: 50px;
+  min-height: 250px;
+}
+h1{
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <footer id="footer">
+            <p>Make Your Burger &copy; 2022</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        name: 'Footer', 
+        data(){}
+    }
+
+</script>
+
+
+<style scoped>
+    #footer{
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+</style>
